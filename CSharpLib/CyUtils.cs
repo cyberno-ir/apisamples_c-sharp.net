00c2b796cc411d223d1aab25ea03fc3965df5070
AmirGooran <gooran@cyberno.ir>
2023-10-07 18:11:11 +0330

    first commit

---
+++ CSharpLib/CyUtils.cs
@@ -0,0 +1,170 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CSharpLib
+{
+    public class CyUtils
+    {
+        private const string USER_AGENT = "Cyberno-API-Sample-CSharp";
+
+        private string server_address;
+        JObject unknownerror_respone_json;
+
+        public CyUtils(string server_address)
+        {
+            this.server_address = server_address;
+
+            this.unknownerror_respone_json = new JObject();
+            this.unknownerror_respone_json.Add("error_code", 900);
+            this.unknownerror_respone_json.Add("success", false);
+        }
+
+        public string get_sha256(string file_path) {
+            using (var sha256_var = SHA256.Create())
+            {
+                using (var stream = File.OpenRead(file_path))
+                {
+                    byte[] file_sha256 = sha256_var.ComputeHash(stream);
+                    StringBuilder Hex = new StringBuilder(file_sha256.Length * 2);
+                    foreach (Byte b in file_sha256)
+                        Hex.AppendFormat("{0:x2}", b);
+                    return Hex.ToString();
+                }
+            }
+        }
+
+        public String get_error(JObject return_value)
+        {
+            String error = "Error!\n";
+            if (return_value.ContainsKey("error_code"))
+                error += ("Error code: " + return_value.SelectToken("error_code").ToObject<string>() + "\n");
+            if (return_value.ContainsKey("error_desc"))
+                error += ("Error description: " + return_value.SelectToken("error_desc").ToObject<string>() + "\n");
+            return error;
+        }
+
+        public JObject call_with_json_input(string api, JObject json_input) {
+            HttpWebRequest HttpWebRequest = (HttpWebRequest)WebRequest.Create(this.server_address + api);
+            HttpWebRequest.ContentType = "application/json";
+            HttpWebRequest.Method = "POST";
+            HttpWebRequest.UserAgent = USER_AGENT;
+            try
+            {
+                using (var streamWriter = new StreamWriter(HttpWebRequest.GetRequestStream()))
+                {
+                    string parsedContent = json_input.ToString();
+                    streamWriter.Write(parsedContent);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+            }
+            catch (Exception)
+            {
+                return unknownerror_respone_json;
+            }
+            string result;
+            try
+            {
+                HttpWebResponse httpResponse = (HttpWebResponse)HttpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                    result = streamReader.ReadToEnd();
+            }
+            catch (WebException ex)
+            {
+                if (ex.Response == null)
+                    return this.unknownerror_respone_json;
+                using (var stream = ex.Response.GetResponseStream())
+                {
+                    using (var reader = new StreamReader(stream))
+                        result = reader.ReadToEnd();
+                }
+            }
+            try
+            {
+                JObject srtr;
+                srtr = JObject.Parse(result);
+                return srtr;
+            }
+            catch (Exception)
+            {
+                return unknownerror_respone_json;
+            }
+        }
+
+        public JObject call_with_form_input(string api, JObject data_input, string file_param_name, string file_path) {
+            string boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x");
+            byte[] boundarybytes = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
+            HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(this.server_address + api);
+            wr.ContentType = "multipart/form-data; boundary=" + boundary;
+            wr.Method = "POST";
+            wr.KeepAlive = true;
+            wr.Headers.Add("UserAgent", USER_AGENT);
+            wr.Credentials = System.Net.CredentialCache.DefaultCredentials;
+            Stream rs = wr.GetRequestStream();
+            string formdataTemplate = "Content-Disposition: form-data; name=\"{0}\"\r\n\r\n{1}";
+            JToken data_input_mover = data_input.First;
+            for (int i = 0; i < data_input.Count; i++)
+            {
+                rs.Write(boundarybytes, 0, boundarybytes.Length);
+                string key = ((JProperty)data_input_mover).Name;
+                string value = data_input_mover.First.Value<string>();
+                string formitem = String.Format(formdataTemplate, key, value);
+                byte[] formitembytes = System.Text.Encoding.UTF8.GetBytes(formitem);
+                rs.Write(formitembytes, 0, formitembytes.Length);
+                data_input_mover = data_input_mover.Next;
+            }
+
+            rs.Write(boundarybytes, 0, boundarybytes.Length);
+            string headerTemplate = "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\n\r\n";
+            string header = String.Format(headerTemplate, file_param_name, "file");
+            byte[] headerbytes = System.Text.Encoding.UTF8.GetBytes(header);
+            rs.Write(headerbytes, 0, headerbytes.Length);
+            FileStream fs = new FileStream(file_path, FileMode.Open, FileAccess.Read);
+            byte[] buffer = new byte[4096];
+            int bytesRead = 0;
+            do
+            {
+                bytesRead = fs.Read(buffer, 0, buffer.Length);
+                if (bytesRead != 0)
+                    rs.Write(buffer, 0, bytesRead);
+            } while (bytesRead != 0);
+            fs.Close();
+
+            byte[] trailer = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "--\r\n");
+            rs.Write(trailer, 0, trailer.Length);
+            rs.Close();
+
+            string result;
+            try
+            {
+                HttpWebResponse httpResponse = (HttpWebResponse)wr.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                    result = streamReader.ReadToEnd();
+            }
+            catch (WebException ex)
+            {
+                if (ex.Response == null)
+                    return unknownerror_respone_json;
+                using (var stream = ex.Response.GetResponseStream())
+                {
+                    using (var reader = new StreamReader(stream))
+                        result = reader.ReadToEnd();
+                }
+            }
+            try
+            {
+                JObject srtr;
+                srtr = JObject.Parse(result);
+                return srtr;
+            }
+            catch (Exception)
+            {
+                return unknownerror_respone_json;
+            }
+        }
+    }
+}
---
+++ exampleUsage/Program.cs
@@ -0,0 +1,174 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace CSharpLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@" ______ ____    ____ .______    _______. ______      .__   __.   ______   
+/      |\   \  /   / |   _  \  |   ____||   _  \     |  \ |  |  /  __  \  
+|,----'  \   \/   /  |  |_)  | |  |__   |  |_)  |    |   \|  | |  |  |  | 
+| |       \_    _/   |   _  <  |   __|  |      /     |. ` |  | |  |  |  | 
+| `----.    |  |     |  |_)  | |  |____ |  |\  \----.|  |\   | |  `--'  | 
+\______|    |__|     |______/  |_______|| _| `._____||__| \__|  \______/  ");
+            string identifier = "", password = "", serveraddress = "";
+            string file_path = "";
+            Console.Write("Please insert API server address [Default=https://multiscannerdemo.cyberno.ir/]: ");
+            serveraddress = Console.ReadLine();
+            if(serveraddress == "")
+                serveraddress = "https://multiscannerdemo.cyberno.ir/";
+            if (serveraddress.EndsWith("/") == false)
+                serveraddress += "/";
+            Console.Write("Please insert identifier (email): ");
+            identifier = Console.ReadLine();
+            Console.Write("Please insert your password: ");
+            password = Console.ReadLine();
+            CyUtils cyutils = new CyUtils(serveraddress);
+            JObject params1 = new JObject();
+            params1.Add("email", identifier);
+            params1.Add("password", password);
+            JObject login_response = cyutils.call_with_json_input("user/login", params1);
+            if (login_response.SelectToken("success").ToObject<bool>() == true)
+                Console.Write("You are logged in successfully.\r\n");
+            else
+            {
+                Console.Write(cyutils.get_error(login_response));
+                Console.ReadLine();
+                return;
+            }
+            string apikey = login_response.SelectToken("data").ToObject<string>();
+            int index;
+            JObject scan_init_response;
+            Console.Clear();
+            Console.WriteLine("Please select scan mode:");
+            Console.WriteLine("1- Scan local folder");
+            Console.WriteLine("2- Scan file");
+            Console.Write("Enter Number=");
+            index = int.Parse(Console.ReadLine());
+            if (index == 1)
+            {
+
+                // Prompt user to enter file paths
+                string paths;
+                string sentenceTwo = (" ");
+                Console.WriteLine("Please enter the paths of file to scan (with spaces): ");
+                paths = Console.ReadLine();
+                sentenceTwo = paths;
+                string[] file_path_array = sentenceTwo.Split(' ');
+
+
+                // Prompt user to enter antivirus names
+                string avs;
+                string avsSentenceTwo = (" ");
+                Console.WriteLine("Enter the name of the selected antivirus (with spaces): ");
+                avs = Console.ReadLine();
+                sentenceTwo = paths;
+                string[] avs_array = avsSentenceTwo.Split(' ');
+
+            
+                // Make Json item
+                JObject scan_item_json = new JObject();
+                scan_item_json.Add("token", apikey);
+                scan_item_json.Add("paths", new JArray(file_path_array));
+                scan_item_json.Add("avs", new JArray(avs_array));
+
+                scan_init_response = cyutils.call_with_json_input("scan/init", scan_item_json);
+                if (scan_init_response.SelectToken("success").ToObject<bool>() == true)
+                    Console.WriteLine(scan_init_response);
+                else
+                {
+                    Console.Write(cyutils.get_error(scan_init_response));
+                    Console.ReadLine();
+                    return;
+                }
+            }
+            else
+            {
+                // Initialize scan
+                Console.Write("Please enter the path of file to scan: ");
+                file_path = Console.ReadLine();
+                string file_name = Path.GetFileName(file_path);
+
+                Console.Write("Enter the name of the selected antivirus (with spaces): ");
+                string avs = Console.ReadLine();
+
+                JObject params2 = new JObject();
+                params2.Add("file", file_name);
+                params2.Add("token", apikey);
+                params2.Add("avs", avs);
+                scan_init_response = cyutils.call_with_form_input("scan/multiscanner/init", params2, "file", file_path);
+                if (scan_init_response.SelectToken("success").ToObject<bool>() == true)
+                    Console.WriteLine(scan_init_response);
+                else
+                {
+                    Console.Write(cyutils.get_error(scan_init_response));
+                    Console.ReadLine();
+                    return;
+                }
+            }
+
+            string guid = scan_init_response["guid"].ToString();
+            if (scan_init_response["success"].ToObject<bool>() == true)
+            {
+                // Get scan response
+                int password_protected = (int)scan_init_response["password_protected"].Count();
+                // Check if password-protected
+                if (password_protected > 0)
+                {
+                    JObject password_item_json = new JObject();
+                    for (int i = 0; i < password_protected; i++)
+                    {
+                        string password_file;
+                        Console.Write("|Enter the Password file -> " + scan_init_response["password_protected"][i] + " |: ");
+                        password_file = Console.ReadLine();
+                        password_item_json["password"] = password_file;
+                        password_item_json["token"] = apikey;
+                        password_item_json["path"] = scan_init_response["password_protected"][i];
+                        cyutils.call_with_json_input("scan/extract/" + guid, password_item_json);
+                    }
+                }
+            }
+            // Start scan
+            Console.WriteLine("=========  Start Scan ===========");
+            JObject scan_json = new JObject();
+            scan_json["token"] = apikey;
+            JObject scan_response = cyutils.call_with_json_input("scan/start/" + guid, scan_json);
+            if (scan_response["success"].ToObject<bool>() == true)
+            {
+                bool is_finished = false;
+                while (!is_finished)
+                {
+                    Console.WriteLine("Waiting for result...");
+                    JObject input_json = new JObject();
+                    input_json["token"] = apikey;
+                    JObject scan_result_response = cyutils.call_with_json_input("scan/result/" + guid, input_json);
+                    try
+                    {
+                        if (scan_result_response["data"]["finished_at"].Value<int>() != 0)
+                        {
+                            is_finished = true;
+                            Console.WriteLine(scan_result_response["data"]);
+                        }
+                    }
+
+                    catch (Exception)
+                    {
+                        Thread.Sleep(5000);
+                        continue;
+                    }
+
+                }
+            }
+            else
+            {
+                Console.WriteLine(cyutils.get_error(scan_response));
+            }
+            return;
+        }
+    }
+}
